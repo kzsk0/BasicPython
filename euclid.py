e10a5f7ec55b18e5c2c8af82123873cd8e504c42
kzsk0 <r.matsumoto.24.4.1311@4nonome.com>
2024-04-16 12:26:48 +0900

    制御構文の解答

--- euclid.py
+++ euclid.py
@@ -1,4 +1,16 @@
-a = input("a の値を入力: ")
-b = input("b の値を入力: ")
+# 問3．ユークリッドの互除法
+
+a = int(input("a の値を入力: "))
+b = int(input("b の値を入力: "))
 
 # TODO
+r = a % b
+
+while r != 0:
+
+  a = b
+  b = r
+  r = a % b
+
+if r == 0:
+  print(b)
--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,28 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+# 問1.　素数判定
+
+a = int(input("aの値を入力: "))
+b = int(input("bの値を入力: "))
 
 # TODO
+def prime_number(n):
+  if n <= 1:
+    return False
+
+  elif n % 2 == 0 or n % 3 == 0:
+    return False
+
+  elif n % 5 == 0 or n % 7 == 0:
+    return False
+
+  else:
+    return True
+
+if prime_number(a):
+    print("aは素数")
+else:
+    print("aは素数ではない")
+
+if prime_number(b):
+    print("bは素数")
+else:
+    print("bは素数ではない")
